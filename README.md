5cfe6f894543243ee9bd31ca14dad88078a532fe
Brandon Davis <neusfear@gmail.com>
2021-12-20 11:27:41 -0600

    Upload file structure

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # chm-website
 Homepage for Country Home Meats Company
+## Getting Started
+
+First, run the development server:
+
+```bash
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Country Home Meats Co</title>
+        <meta name="description" content="Homepage of the Country Home Meats Company" />
+      </Head>
+
+      <main className="text-6xl">
+        Hello World
+      </main>
+
+      <footer>
+        todo
+      </footer>
+    </>
+  )
+}
+
+export default Home
