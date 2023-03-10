c12456a3a40c09fd0a0d0a7d1c924edab5ada226
Brandon Davis <neusfear@gmail.com>
2023-03-10 14:09:08 -0600

    Maker prices formatted to dollar amounts

--- components/utility/PricesList.tsx
+++ components/utility/PricesList.tsx
@@ -22,7 +22,7 @@ const PriceItem = ({ priceItem }: { priceItem: RetailPriceType }) => {
   return (
     <tr className="border-neutral-300 border-b even:bg-stone-100 hover:bg-red-700 hover:bg-opacity-10">
       <td>{priceItem.item}</td>
-      <td className="text-center">{priceItem.price === -1 ? "Call for Pricing" : "$" + priceItem.price }</td>
+      <td className="text-center">{priceItem.price === -1 ? "Call for Pricing" : "$" + priceItem.price.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') }</td>
       <td>{priceItem.unit}</td>
     </tr>
   );
