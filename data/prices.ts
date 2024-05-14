1fb15e39a2952eb52ffa124ce77d79c93f51f113
jesseed123 <107962184+jesseed123@users.noreply.github.com>
2024-05-14 11:32:49 -0500

    Update prices.ts

--- data/prices.ts
+++ data/prices.ts
@@ -37,7 +37,7 @@ export const retailPorkPrices: RetailPriceType[] = [
   { 'item': "Pork Chops", 'price': 3.79, 'unit': "Per Lb." },
   { 'item': "Pork Steak", 'price': 2.89, 'unit': "Per Lb." },
   { 'item': "Pork Tenderloin", 'price': 4.99, 'unit': "Per Lb." },
-  { 'item': "Spare Ribs", 'price': 2.99, 'unit': "Per Lb." },
+  { 'item': "Spare Ribs", 'price': 3.49, 'unit': "Per Lb." },
   { 'item': "Baby Back Ribs", 'price': 4.99, 'unit': "Per Lb." },
   { 'item': "Hot Sausage", 'price': 3.89, 'unit': "Per Lb." },
   { 'item': "Regular Sausage", 'price': 3.89, 'unit': "Per Lb." },
