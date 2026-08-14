aa46fc3f08e78f5056100462ab102c985c7871c6
jesseed123 <107962184+jesseed123@users.noreply.github.com>
2026-08-14 11:22:49 -0500

    Update WelcomeBanner.tsx

--- components/page/WelcomeBanner.tsx
+++ components/page/WelcomeBanner.tsx
@@ -6,7 +6,7 @@ const WelcomeBanner = () => {
             <BackgroundImage alt="banner" src="/images/front.jpg" />
             <div className="bg-gray-900 bg-opacity-70 absolute w-full h-full top-0 text-white text-center flex flex-col place-content-center">
                 <h1 className="text-7xl font-bold" >COUNTRY HOME MEATS</h1>
-                <p className="text-xl">Your local source for Retail, Custom Processed Meats, <br />and Inspected Slaughter in Central Oklahoma. <br /> WE WILL BE CLOSED JULY 3RD THROUGH JULY 12TH FOR VACATION. </p>                
+                <p className="text-xl">Your local source for Retail, Custom Processed Meats, <br />and Inspected Slaughter in Central Oklahoma. </p>                
             </div>
         </div>
     );
